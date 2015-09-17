4bdee0e0891fc2241298719499057d0bbc3f5151
Thomas Angelbo Christensen <thomas@eject.io>
2015-09-17 19:03:31 +0200

    Grid

---
+++ grid/gulpfile.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+var gulp = require("gulp");
+var runSequence = require("run-sequence");
+var sass = require("gulp-sass");
+
+
+gulp.task("scss-compile", function ()
+{
+    return gulp
+    .src("./scss/index.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist"));
+});
+
+
+gulp.task("build", function ()
+{
+    runSequence("scss-compile");
+});
+
+
+gulp.task("default", function ()
+{
+    gulp.run("build");
+
+    gulp.watch([], ["build"]);
+});
