2743b039b28c083a4c3f19ea57ab6aaacdb8b2dd
Thomas Angelbo Christensen <thomas@eject.io>
2015-11-10 22:24:35 +0100

    wip

--- ssl-localhost/index.js
+++ ssl-localhost/index.js
@@ -35,50 +35,55 @@ $(function ()
         "h1, h2, label, li, input, a, span",
         {
             // top level headers
-            "Configure Your Plan:":     "___Configure Your Plan:",
-            "Purchase Summary:":        "___Purchase Summary:",
-            "Tax Information:":         "___Tax Information:",
-            "Billing Information":      "___Billing Information",
-            "Billing Address:":         "___Billing Address:",
-            "Contact Information:":     "___Contact Information:",
+            "Configure Your Plan:":     "Tilkøb point og services",
+            "Purchase Summary:":        "Purchase Summary",
+            "Tax Information:":         "Tax Information",
+            "Billing Information":      "Billing Information",
+            "Billing Address:":         "Billing Address",
+            "Contact Information:":     "Contact Information",
 
             // errors
-            "There were problems with your submission":         "___There were problems with your submission",
-            "Please correct the following problems:":           "___Please correct the following problems:",
-            "First name: cannot be blank.":                     "___First name: cannot be blank.",
-            "Last name: cannot be blank.":                      "___Last name: cannot be blank.",
-            "Email address: cannot be blank.":                  "___Email address: cannot be blank.",
-            "Credit card expiration month: cannot be blank.":   "___Credit card expiration month: cannot be blank.",
-            "Credit card expiration year: cannot be blank.":    "___Credit card expiration year: cannot be blank.",
-            "Credit card number: cannot be blank.":             "___Credit card number: cannot be blank.",
+            "There were problems with your submission":         "There were problems with your submission",
+            "Please correct the following problems:":           "Please correct the following problems",
+            "First name: cannot be blank.":                     "First name: cannot be blank.",
+            "Last name: cannot be blank.":                      "Last name: cannot be blank.",
+            "Email address: cannot be blank.":                  "Email address: cannot be blank.",
+            "Credit card expiration month: cannot be blank.":   "Credit card expiration month: cannot be blank.",
+            "Credit card expiration year: cannot be blank.":    "Credit card expiration year: cannot be blank.",
+            "Credit card number: cannot be blank.":             "Credit card number: cannot be blank.",
 
             // buttons
-            "Update Totals":        "___Update Totals",
-            "Validate VAT Number":  "___Validate VAT Number",
-            "Place My Order":       "___Place My Order",
+            "Update Totals":        "Update Totals",
+            "Validate VAT Number":  "Validate VAT Number",
+            "Place My Order":       "Place My Order",
 
             // misc
-            "Residents of the EU, outside of DK, may enter their VAT number to be exempt from tax.":    "___Residents of the EU, outside of DK, may enter their VAT number to be exempt from tax.",
-            "Please enter your VAT number without the country code":                                    "___Please enter your VAT number without the country code",
+            "Residents of the EU, outside of DK, may enter their VAT number to be exempt from tax.":    "Residents of the EU, outside of DK, may enter their VAT number to be exempt from tax.",
+            "Please enter your VAT number without the country code":                                    "Please enter your VAT number without the country code",
 
             // labels
-            "VAT Number":               "___VAT Number",
-            "* First Name on Account":  "___* First Name on Account",
-            "* Last Name on Account":   "___* Last Name on Account",
-            "* Card Number":            "___* Card Number",
-            "* CVV":                    "___* CVV",
-            "* Expiration Date":        "___* Expiration Date",
-            "* Address 1":              "___* Address 1",
-            "Address 2":                "___Address 2",
-            "* Billing Country":        "___* Billing Country",
-            "* Billing State":          "___* Billing State",
-            "* Billing City":           "___* Billing City",
-            "* Billing ZIP Code":       "___* Billing ZIP Code",
-            "* First Name":             "___* First Name",
-            "* Last Name":              "___* Last Name",
-            "* Email Address":          "___* Email Address",
-            "Phone":                    "___Phone",
-            "Organization":             "___Organization"
+            "VAT Number":               "VAT Number",
+            "* First Name on Account":  "First Name on Account",
+            "* Last Name on Account":   "Last Name on Account",
+            "* Card Number":            "Card Number",
+            "* CVV":                    "CVV",
+            "* Expiration Date":        "Expiration Date",
+            "* Address 1":              "Address 1",
+            "Address 2":                "Address 2",
+            "* Billing Country":        "Billing Country",
+            "* Billing State":          "Billing State",
+            "* Billing City":           "Billing City",
+            "* Billing ZIP Code":       "Billing ZIP Code",
+            "* First Name":             "First Name",
+            "* Last Name":              "Last Name",
+            "* Email Address":          "Email Address",
+            "Phone":                    "Phone",
+            "Organization":             "Organization",
+
+            // unit price fix
+            "kr1,350.00": "1.350,00 DKK",
+            "kr2,000.00": "2.000,00 DKK",
+            "Add for kr19.00": "Tilføj til 19,00 DKK per måned"
         }
     );
 
@@ -90,7 +95,7 @@ $(function ()
             "h2, span",
             {
                 // top level headers
-                "Purchase Summary:": "___Purchase Summary:"
+                "Purchase Summary:": "Purchase Summary"
             }
         );
     });
